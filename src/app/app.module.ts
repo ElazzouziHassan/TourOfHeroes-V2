ad81f415820388a20b8f8e529c829ea3fe5cfee1
wizardy <wizardhassan99@gmail.com>
2024-01-13 21:21:00 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './component/heroes/heroes.component';
+import { CrisisComponent } from './component/crisis/crisis.component';
+import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
+import { CrisisListComponent } from './pages/crisis-list/crisis-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    CrisisComponent,
+    HeroesListComponent,
+    CrisisListComponent
   ],
   imports: [
     BrowserModule,
