8787d25be3605f8398f8a2b8b1828ed735261c14
wizardy <wizardhassan99@gmail.com>
2024-01-13 21:21:17 +0100

    commit

---
+++ src/app/component/crisis/crisis.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crisis',
+  templateUrl: './crisis.component.html',
+  styleUrl: './crisis.component.css'
+})
+export class CrisisComponent {
+
+}
