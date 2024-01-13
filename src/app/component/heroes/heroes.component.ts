8fbde1d9d298028df71299ce3cc7e28134288d11
wizardy <wizardhassan99@gmail.com>
2024-01-13 21:41:07 +0100

    commit

--- src/app/component/heroes/heroes.component.ts
+++ src/app/component/heroes/heroes.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import {HEROES} from '../../classes/hero-list';
+// import {HEROES} from '../../classes/hero-list';
+import { Hero } from '../../classes/hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  // heroes = HEROES;
+  hero: Hero = {
+    id: 1,
+    name: 'Jhon'
+  }
 }
