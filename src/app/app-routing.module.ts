e14c0331a7b34a0dd241cc091359738dab161985
wizardy <wizardhassan99@gmail.com>
2024-01-14 12:21:28 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
+import { CrisisListComponent } from './pages/crisis-list/crisis-list.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  
+  {path: 'crisis', title:'Tour Of Heroes | Crisis', component: CrisisListComponent},
+  {path: 'heroes', title:'Tour Of Heroes | Heroes', component: HeroesListComponent},
+  {path: '', redirectTo: 'heroes', pathMatch: 'full'},
+  {path: '**', title:'Error ! | Page Not Found', component: PageNotFoundComponent},
 ];
 
 @NgModule({
